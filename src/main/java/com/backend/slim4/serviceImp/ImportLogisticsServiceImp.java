ffed9cd96ba05f0f4c2fc81e781a93b37c58de78
unknown <mateo.sabogal@benitomo.com>
2021-03-31 13:13:20 -0500

    ImportLogistics finalizada

--- src/main/java/com/backend/slim4/serviceImp/ImportLogisticsServiceImp.java
+++ src/main/java/com/backend/slim4/serviceImp/ImportLogisticsServiceImp.java
@@ -63,22 +63,26 @@ public class ImportLogisticsServiceImp implements ImportLogisticsService {
                 i.setPlcDate(rs.getDate("plcdate"));
                 i.setPlcPerc(rs.getBigDecimal("plcperc"));
                 i.setAbcClass(rs.getString("abcclass"));
-                i.setBuyingPrice(rs.getBigDecimal("buyinprice"));
+                i.setBuyingPrice(rs.getBigDecimal("buyingprice"));
                 i.setMSQ(rs.getInt("msq"));
                 i.setISQ(rs.getInt("isq"));
                 logistics.add(i);
             }
             cnt.close();
         } catch (SQLException ex) {
-            Logger.getLogger(ArticleFilterServiceImp.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ImportLogisticsServiceImp.class.getName()).log(Level.SEVERE, null, ex);
         }
         if(logistics.size()>0){
             try {
                 return importLogisticsInsert(logistics);
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ArticleFilterServiceImp.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ImportLogisticsServiceImp.class.getName()).log(Level.SEVERE, null, ex);
             }
             
+        } else {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("Datos no encontrados", "la tabla logistics está vacía");
+            return new ResponseEntity(map, HttpStatus.OK);
         }
         
         HashMap<String, String> map = new HashMap<>();
@@ -109,7 +113,7 @@ public class ImportLogisticsServiceImp implements ImportLogisticsService {
     }
     
     public int emptyTable(Statement stmt){
-        String sql = "delete from S4Import_Logistics";
+        String sql = "delete from [slim4interface_test].[dbo].[S4Import_Logistics]";
         int result = 0;
         try {
             result = stmt.executeUpdate(sql);
@@ -119,27 +123,27 @@ public class ImportLogisticsServiceImp implements ImportLogisticsService {
         return result;
     } 
     
-    public int insertDataTable(Statement stmt, ArrayList<ImportLogistics> a) throws SQLException{
-        String sql = "SET NOCOUNT ON INSERT INTO S4Import_Logistics(controlId, warehouse, code) SELECT * FROM  (VALUES";
-            for (int i = 0; i < a.size(); i++) {
-                sql =  (i==a.size()-1)? 
-                            sql + "(" + a.get(i).getControlId() + ", '"+ a.get(i).getWarehousecode() +"', '" + a.get(i).getCode() + "')) AS temporal(controlId, warehouse, code)"
+    public int insertDataTable(Statement stmt, ArrayList<ImportLogistics> il) throws SQLException{
+        String sql = "SET NOCOUNT ON INSERT INTO [slim4interface_test].[dbo].[S4Import_Logistics](controlId,warehouse,code,supplierNumber,supplierName,leadTime,reviewTime,supplierReliability,supplierReliabilityType,stockedItem,MOQ,IOQ,EOQ,logisticUnit1,logisticUnit2,logisticUnit3,logisticUnit4,logisticUnit5,logisticUnit6,insuranceInventory,insuranceInventoryType,targetServiceLevel,plcArticleCode,plcDate,plcPerc,abcClass,buyingPrice,MSQ,ISQ) SELECT * FROM  (VALUES";
+            for (int i = 0; i < il.size(); i++) {
+                sql =  (i==il.size()-1)? 
+                            sql + "(" + il.get(i).getControlId() + ", '"+ il.get(i).getWarehousecode() +"', '" + il.get(i).getCode() + "', '" + il.get(i).getSupplierNumber()+ "', '" + il.get(i).getSupplierName()+ "', '" + il.get(i).getLeadTime()+ "', '" + il.get(i).getReviewTime()+ "', '" + il.get(i).getSupplierReliability()+ "', '" + il.get(i).getSupplierReliabilityType()+ "', '" + il.get(i).getStockedItem()+ "', '" + il.get(i).getMOQ()+ "', '" + il.get(i).getIOQ()+ "', '" + il.get(i).getEOQ()+ "', '" + il.get(i).getLogisticUnit1()+ "', '" + il.get(i).getLogisticUnit2()+ "', '" + il.get(i).getLogisticUnit3()+ "', '" + il.get(i).getLogisticUnit4()+ "', '" + il.get(i).getLogisticUnit5()+ "', '" + il.get(i).getLogisticUnit6()+ "', '" + il.get(i).getInsuranceInventory()+ "', '" + il.get(i).getInsuranceInventoryType()+ "', '" + il.get(i).getTargetServiceLevel()+ "', '" + il.get(i).getPlcArticleCode()+ "', '" + il.get(i).getPlcDate()+ "', '" + il.get(i).getPlcPerc()+ "', '" + il.get(i).getAbcClass()+ "', '" + il.get(i).getBuyingPrice()+ "', '" + il.get(i).getMSQ()+ "', '" + il.get(i).getISQ()+ "')) as temporal(controlId,warehouse,code,supplierNumber,supplierName,leadTime,reviewTime,supplierReliability,supplierReliabilityType,stockedItem,MOQ,IOQ,EOQ,logisticUnit1,logisticUnit2,logisticUnit3,logisticUnit4,logisticUnit5,logisticUnit6,insuranceInventory,insuranceInventoryType,targetServiceLevel,plcArticleCode,plcDate,plcPerc,abcClass,buyingPrice,MSQ,ISQ)"
                             :
-                            sql + "(" + a.get(i).getControlId() + ", '"+ a.get(i).getWarehousecode() +"', '" + a.get(i).getCode() + "'),";
+                            sql + "(" + il.get(i).getControlId() + ", '"+ il.get(i).getWarehousecode() +"', '" + il.get(i).getCode() + "', '" + il.get(i).getSupplierNumber()+ "', '" + il.get(i).getSupplierName()+ "', '" + il.get(i).getLeadTime()+ "', '" + il.get(i).getReviewTime()+ "', '" + il.get(i).getSupplierReliability()+ "', '" + il.get(i).getSupplierReliabilityType()+ "', '" + il.get(i).getStockedItem()+ "', '" + il.get(i).getMOQ()+ "', '" + il.get(i).getIOQ()+ "', '" + il.get(i).getEOQ()+ "', '" + il.get(i).getLogisticUnit1()+ "', '" + il.get(i).getLogisticUnit2()+ "', '" + il.get(i).getLogisticUnit3()+ "', '" + il.get(i).getLogisticUnit4()+ "', '" + il.get(i).getLogisticUnit5()+ "', '" + il.get(i).getLogisticUnit6()+ "', '" + il.get(i).getInsuranceInventory()+ "', '" + il.get(i).getInsuranceInventoryType()+ "', '" + il.get(i).getTargetServiceLevel()+ "', '" + il.get(i).getPlcArticleCode()+ "', '" + il.get(i).getPlcDate()+ "', '" + il.get(i).getPlcPerc()+ "', '" + il.get(i).getAbcClass()+ "', '" + il.get(i).getBuyingPrice()+ "', '" + il.get(i).getMSQ()+ "', '" + il.get(i).getISQ()+ "'),";
             }
         return stmt.executeUpdate(sql);
     } 
     
     public ArrayList<ImportLogistics> getArticleFilterInfo(Statement stmt) throws SQLException{
-        ArrayList<ImportLogistics> articles = new ArrayList<>();
-        ResultSet rs = stmt.executeQuery("select * from S4Import_Logistics");
+        ArrayList<ImportLogistics> logistics = new ArrayList<>();
+        ResultSet rs = stmt.executeQuery("select * from [slim4interface_test].[dbo].[S4Import_Logistics]");
             while (rs.next()) {
                 ImportLogistics ar = new ImportLogistics();
                 ar.setControlId(rs.getInt("controlId"));
                 ar.setWarehousecode(rs.getString("warehouse"));
                 ar.setCode(rs.getString("code"));
-                articles.add(ar);
+                logistics.add(ar);
             }
-        return articles;    
+        return logistics;    
     }
 }
