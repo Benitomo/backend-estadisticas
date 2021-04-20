4cd92e524ec5740a3d09d2925ab598317e154d86
unknown <mateo.sabogal@benitomo.com>
2021-04-20 15:25:52 -0500

    ArticleCodeMaster en proceso

--- src/main/java/com/backend/slim4/serviceImp/ArticleCodeMasterServiceImp.java
+++ src/main/java/com/backend/slim4/serviceImp/ArticleCodeMasterServiceImp.java
@@ -79,40 +79,222 @@ public class ArticleCodeMasterServiceImp implements ArticleCodeMasterService{
                     + "audfield20"
                     + "audfield21"
                     + "audfield22"
-                    + "from stockdetails";
+                    + "audfield23"
+                    + "audfield24"
+                    + "audfield25"
+                    + "audfield26"
+                    + "audfield27"
+                    + "audfield28"
+                    + "audfield29"
+                    + "audfield30"
+                    + "audfield31"
+                    + "audfield32"
+                    + "audfield33"
+                    + "audfield34"
+                    + "audfield35"
+                    + "audfield36"
+                    + "audfield37"
+                    + "audfield38"
+                    + "audfield38"
+                    + "audfield39"
+                    + "audfield40"
+                    + "audfield41"
+                    + "audfield42"
+                    + "audfield43"
+                    + "audfield44"
+                    + "audfield45"
+                    + "audfield46"
+                    + "audfield47"
+                    + "audfield48"
+                    + "audfield49"
+                    + "audfield50"
+                    + "audfield51"
+                    + "audfield52"
+                    + "audfield53"
+                    + "audfield54"
+                    + "audfield55"
+                    + "audfield56"
+                    + "audfield57"
+                    + "audfield58"
+                    + "audfield59"
+                    + "audfield60"
+                    + "audfield61"
+                    + "audfield62"
+                    + "audfield63"
+                    + "audfield64"
+                    + "audfield65"
+                    + "audfield66"
+                    + "audfield67"
+                    + "audfield68"
+                    + "audfield69"
+                    + "audfield70"
+                    + "audfield71"
+                    + "audfield72"
+                    + "audfield73"
+                    + "audfield74"
+                    + "audfield75"
+                    + "audfield76"
+                    + "audfield77"
+                    + "audfield78"
+                    + "audfield79"
+                    + "audfield80"
+                    + "audfield81"
+                    + "audfield82"
+                    + "audfield83"
+                    + "audfield84"
+                    + "audfield85"
+                    + "audfield86"
+                    + "audfield87"
+                    + "audfield88"
+                    + "audfield89"
+                    + "audfield90"
+                    + "audfield91"
+                    + "audfield92"
+                    + "audfield93"
+                    + "audfield94"
+                    + "audfield95"
+                    + "audfield96"
+                    + "audfield97"
+                    + "audfield98"
+                    + "audfield99"
+                    + "audfield100"
+                    + "from articlecodemaster";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 ArticleCodeMaster m = new ArticleCodeMaster();
                 m.setControlId(rs.getInt("controlid"));
-                m.setTransactionNumber(rs.getString("transactionnumber"));
-                m.setTransactionType(rs.getString("transactiontype"));
-                m.setTransactionName(rs.getString("transactionname"));
-                m.setTransactionName(rs.getString("transactionstatus"));
-                m.setWarehouse(rs.getString("warehouse"));
+                m.setWarehousecode(rs.getString("warehouse"));
                 m.setCode(rs.getString("articlecode"));
-                m.setIssueDate(rs.getDate("issuedate"));
-                m.setConfirmedDate(rs.getDate("confirmeddate"));
-                m.setRequestedDate(rs.getDate("requesteddate"));
-                m.setIssueQuantity(rs.getBigDecimal("issuequantity"));
-                m.setLineNumber(rs.getString("linenumber"));
-                m.setConfirmedQuantity(rs.getBigDecimal("confirmedquantity"));
-                m.setRequestedQuantity(rs.getBigDecimal("requestedquantity"));
-                m.setCustomerNumber(rs.getString("customernumber"));
-                m.setCustomerType(rs.getString("customertype"));
-                m.setCustomerName(rs.getString("customername"));
+                m.setCreationDate(rs.getDate("creationdate"));
+                m.setDescription(rs.getString("description"));
+                m.setUnitPrice(rs.getBigDecimal("unitprice"));
                 m.setSalesPrice(rs.getBigDecimal("salesprice"));
-                m.setDeliveryLocation(rs.getString("deliverylocation"));
-                m.setSupplierNumber(rs.getString("suppliernumber"));
-                m.setSupplierType(rs.getString("suppliertype"));
-                m.setSupplierName(rs.getString("suppliername"));
-                m.setBuyingPrice(rs.getBigDecimal("buyingprice"));
-                m.setSupplyingLocation(rs.getString("supplyinglocation"));
-                m.setConversionFactor(rs.getBigDecimal("conversionfactor"));
+                m.setCriterium1(rs.getBigDecimal("criterium1"));
+                m.setCriterium1(rs.getBigDecimal("criterium2"));
+                m.setCriterium1(rs.getBigDecimal("criterium3"));
+                m.setCriterium1(rs.getBigDecimal("criterium4"));
+                m.setGroupCode1(rs.getString("groupcode1"));
+                m.setGroupCode1(rs.getString("groupcode2"));
+                m.setGroupCode1(rs.getString("groupcode3"));
+                m.setGroupCode1(rs.getString("groupcode4"));
+                m.setGroupCode1(rs.getString("groupcode5"));
+                m.setGroupCode1(rs.getString("groupcode6"));
                 m.setuD1(rs.getString("ud1"));
                 m.setuD1(rs.getString("ud2"));
                 m.setuD1(rs.getString("ud3"));
                 m.setuD1(rs.getString("ud4"));
-                m.setTransactionIssueTime(rs.getString("transactionissuetime"));
+                m.setuD1(rs.getString("ud5"));
+                m.setuD1(rs.getString("ud6"));
+                m.setuD1(rs.getString("ud7"));
+                m.setuD1(rs.getString("ud8"));
+                m.setuD1(rs.getString("ud9"));
+                m.setuD1(rs.getString("ud10"));
+                m.setuD1(rs.getString("ud11"));
+                m.setuD1(rs.getString("ud12"));
+                m.setuD1(rs.getString("ud13"));
+                m.setuD1(rs.getString("ud14"));
+                m.setuD1(rs.getString("ud15"));
+                m.setuD1(rs.getString("audfield1"));
+                m.setuD1(rs.getString("audfield2"));
+                m.setuD1(rs.getString("audfield3"));
+                m.setuD1(rs.getString("audfield4"));
+                m.setuD1(rs.getString("audfield5"));
+                m.setuD1(rs.getString("audfield6"));
+                m.setuD1(rs.getString("audfield7"));
+                m.setuD1(rs.getString("audfield8"));
+                m.setuD1(rs.getString("audfield9"));
+                m.setuD1(rs.getString("audfield10"));
+                m.setuD1(rs.getString("audfield11"));
+                m.setuD1(rs.getString("audfield12"));
+                m.setuD1(rs.getString("audfield13"));
+                m.setuD1(rs.getString("audfield14"));
+                m.setuD1(rs.getString("audfield15"));
+                m.setuD1(rs.getString("audfield16"));
+                m.setuD1(rs.getString("audfield17"));
+                m.setuD1(rs.getString("audfield18"));
+                m.setuD1(rs.getString("audfield19"));
+                m.setuD1(rs.getString("audfield20"));
+                m.setuD1(rs.getString("audfield21"));
+                m.setuD1(rs.getString("audfield22"));
+                m.setuD1(rs.getString("audfield23"));
+                m.setuD1(rs.getString("audfield23"));
+                m.setuD1(rs.getString("audfield24"));
+                m.setuD1(rs.getString("audfield25"));
+                m.setuD1(rs.getString("audfield26"));
+                m.setuD1(rs.getString("audfield27"));
+                m.setuD1(rs.getString("audfield28"));
+                m.setuD1(rs.getString("audfield29"));
+                m.setuD1(rs.getString("audfield30"));
+                m.setuD1(rs.getString("audfield31"));
+                m.setuD1(rs.getString("audfield32"));
+                m.setuD1(rs.getString("audfield33"));
+                m.setuD1(rs.getString("audfield34"));
+                m.setuD1(rs.getString("audfield35"));
+                m.setuD1(rs.getString("audfield36"));
+                m.setuD1(rs.getString("audfield37"));
+                m.setuD1(rs.getString("audfield38"));
+                m.setuD1(rs.getString("audfield39"));
+                m.setuD1(rs.getString("audfield40"));
+                m.setuD1(rs.getString("audfield41"));
+                m.setuD1(rs.getString("audfield42"));
+                m.setuD1(rs.getString("audfield43"));
+                m.setuD1(rs.getString("audfield44"));
+                m.setuD1(rs.getString("audfield45"));
+                m.setuD1(rs.getString("audfield46"));
+                m.setuD1(rs.getString("audfield47"));
+                m.setuD1(rs.getString("audfield48"));
+                m.setuD1(rs.getString("audfield49"));
+                m.setuD1(rs.getString("audfield50"));
+                m.setuD1(rs.getString("audfield51"));
+                m.setuD1(rs.getString("audfield52"));
+                m.setuD1(rs.getString("audfield53"));
+                m.setuD1(rs.getString("audfield54"));
+                m.setuD1(rs.getString("audfield55"));
+                m.setuD1(rs.getString("audfield56"));
+                m.setuD1(rs.getString("audfield57"));
+                m.setuD1(rs.getString("audfield58"));
+                m.setuD1(rs.getString("audfield59"));
+                m.setuD1(rs.getString("audfield60"));
+                m.setuD1(rs.getString("audfield61"));
+                m.setuD1(rs.getString("audfield62"));
+                m.setuD1(rs.getString("audfield63"));
+                m.setuD1(rs.getString("audfield64"));
+                m.setuD1(rs.getString("audfield65"));
+                m.setuD1(rs.getString("audfield66"));
+                m.setuD1(rs.getString("audfield67"));
+                m.setuD1(rs.getString("audfield68"));
+                m.setuD1(rs.getString("audfield69"));
+                m.setuD1(rs.getString("audfield70"));
+                m.setuD1(rs.getString("audfield71"));
+                m.setuD1(rs.getString("audfield72"));
+                m.setuD1(rs.getString("audfield73"));
+                m.setuD1(rs.getString("audfield74"));
+                m.setuD1(rs.getString("audfield75"));
+                m.setuD1(rs.getString("audfield76"));
+                m.setuD1(rs.getString("audfield77"));
+                m.setuD1(rs.getString("audfield78"));
+                m.setuD1(rs.getString("audfield79"));
+                m.setuD1(rs.getString("audfield80"));
+                m.setuD1(rs.getString("audfield81"));
+                m.setuD1(rs.getString("audfield82"));
+                m.setuD1(rs.getString("audfield83"));
+                m.setuD1(rs.getString("audfield84"));
+                m.setuD1(rs.getString("audfield85"));
+                m.setuD1(rs.getString("audfield86"));
+                m.setuD1(rs.getString("audfield87"));
+                m.setuD1(rs.getString("audfield88"));
+                m.setuD1(rs.getString("audfield89"));
+                m.setuD1(rs.getString("audfield90"));
+                m.setuD1(rs.getString("audfield91"));
+                m.setuD1(rs.getString("audfield92"));
+                m.setuD1(rs.getString("audfield93"));
+                m.setuD1(rs.getString("audfield94"));
+                m.setuD1(rs.getString("audfield95"));
+                m.setuD1(rs.getString("audfield96"));
+                m.setuD1(rs.getString("audfield97"));
+                m.setuD1(rs.getString("audfield98"));
+                m.setuD1(rs.getString("audfield99"));
+                m.setuD1(rs.getString("audfield100"));
                 master.add(m);
             }
             cnt.close();
